dfe5938c89b30cf500e559ad7581b73122ed7054
Kyle Rigler <kylelrigler@gmail.com>
2022-10-08 16:08:00 -0400

    ex2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            // Method Syntax
+            Console.WriteLine("------Method Syntax------");
+            List<string> Games = new List<string>
+            {
+                "call of duty warzone",//20
+                "rocket leage",//12
+                "elden ring",//10
+                "ark" //1
+            };
+            //--------------------------------\\
+
+            IEnumerable<String> Order = Games.OrderBy(x => x.Length);// took me so long to see i needed .length
+             foreach (string Game in Order)
+             {
+                Console.WriteLine(Game);
+             }
+             Console.WriteLine("-------------------------");
+            //Query Syntax
+            Console.WriteLine("------Query Syntax-------");
+            List<string> Gamestwo = new List<string>
+            {
+                "call of duty warzone",//20
+                "rocket leage",//12
+                "elden ring",//10
+                "ark" //1
+            };
+            var Length = from GameX in Gamestwo
+                         orderby GameX.Length ascending
+                         select GameX;
+            foreach (string GameX in Length)
+            {
+                Console.WriteLine(GameX);
+            }
+            Console.WriteLine("---------------------------");
+            
+
+
+
+
+
+
+
         }
     }
 }
